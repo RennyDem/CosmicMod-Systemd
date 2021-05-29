e118326c4637ab9bbb1fb50e12b404b20c249d85
Oz <oz@cosmicmod.net>
2021-05-28 21:48:48 -0500

    Update 2-chroot-build.sh

--- build-scripts/2-chroot-build.sh
+++ build-scripts/2-chroot-build.sh
@@ -35,9 +35,6 @@ emerge --ask --verbose --update --deep --newuse --with-bdeps=y @world
 # Host-env
 # vim /mnt/gentoo/etc/portage/package.use/circular.use
 
-emerge --sync &&
-echo 'Updating the system in 1 minute' &&
-sleep 1m &&
 emerge --ask --verbose --update --deep --newuse --with-bdeps=y @world
 
 paperconfig -p letter
