71e14bcc261f902a094a2877e96c8c00f761a9fa
Oz <oz@cosmicmod.net>
2021-05-30 23:22:02 -0500

    Update 2-chroot-build.sh

--- build-scripts/2-chroot-build.sh
+++ build-scripts/2-chroot-build.sh
@@ -86,9 +86,7 @@ echo 'news successfully read and purged'
 # host-env
 # cp --dereference /etc/conf.d/display-manager /mnt/gentoo/etc/conf.d/
 
-usermod -a -G video sddm &&
-emerge --ask --oneshot --quiet virtual/jre &&
-eselect java-vm show
+usermod -a -G video sddm
 
 # host-env
 # cp --dereference /var/lib/portage/world /mnt/gentoo/var/lib/portage/
