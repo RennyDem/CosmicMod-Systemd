578353df7c8490ceb1c5d4381e9780002a188ab0
Oz <oz@cosmicmod.net>
2021-06-20 17:25:05 -0500

    Update 2-chroot-build.sh

--- build-scripts/2-chroot-build.sh
+++ build-scripts/2-chroot-build.sh
@@ -81,7 +81,7 @@ cp --dereference /var/lib/portage/world /mnt/gentoo/var/lib/portage/
 
 usermod -a -G video sddm &&
 emerge --ask --sync --quiet &&
-echo 'Updating system in 1m.  Press ctrl+c to stop' &&
+echo 'Updating system in 1m.  Press ctrl+c to stop.' &&
 sleep 1m &&
 emerge --ask --verbose --quiet --update --deep --newuse --with-bdeps=y @world
 
@@ -93,17 +93,23 @@ revdep-rebuild
 
 eselect fontconfig list
 
-eselect fontconfig enable 1 2 3 4 6 8 9 10 11 12 13 14 15 16 17 18 20 28 29 30 33 35 37 38 39 41 42 &&
-eselect fontconfig list &&
-sleep 1m &&
+eselect fontconfig enable \
+1 2 3 4 5 6 7 8 9 10 \
+11 12 13 14 15 16 17 18 19 20 \
+21 22 23 24 25 26 27 28 29 30 \
+31 32 33 34 35 36 37 38 39 40 \
+41 42 43 44 45 46 47 48 49 50 \
+51 52 53 54 55 56 57 58 59 60 \
+61 62 63 64 65 66 67 68 69 70
+
 env-update &&
 source /etc/profile &&
 export PS1="(chroot) $PS1" &&
 emerge --ask --depclean &&
-echo 'cleaning old distfiles in 1m' &&
+echo 'Cleaning old distfiles in 1m...' &&
 sleep 1m &&
 eclean -d distfiles &&
-echo 'script successfully completed'
+echo 'Script successfully completed :P'
 
 vim /etc/resolv.conf
 
