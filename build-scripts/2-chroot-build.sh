a2bd573c0d65aaa97a86a88d2c208b1671a999cd
RennyDem <59265470+RennyDem@users.noreply.github.com>
2023-03-17 02:22:40 +0300

    added commands to install steam and lutris via flatpak

--- build-scripts/2-chroot-build.sh
+++ build-scripts/2-chroot-build.sh
@@ -70,7 +70,9 @@ emerge --ask --quiet --sync &&
 emerge --ask --verbose --quiet kde-plasma/plasma-meta &&
 emerge --ask sys-apps/flatpak &&
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
-flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo &&
+flatpak install flathub com.valvesoftware.Steam && 
+flatpak install flathub net.lutris.Lutris
 
 vim /etc/portage/package.use/circular.use
 # add if needed
